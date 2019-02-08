89026c5eb2058d9b4cafccdc43e37ea4f9bdba23
Natenael Kelkay <natikelkay@gmail.com>
2019-02-08 16:15:10 -0500

    made the calc component extend limeLightComponent

--- src/main/java/org/usfirst/frc/team449/robot/components/LimeLightAngularToRelevantComponent.java
+++ src/main/java/org/usfirst/frc/team449/robot/components/LimeLightAngularToRelevantComponent.java
@@ -4,26 +4,20 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import org.jetbrains.annotations.NotNull;
-import org.usfirst.frc.team449.robot.subsystem.interfaces.analogMotor.commands.AnalogPIDCommand;
 
 import java.util.function.DoubleSupplier;
 
 @JsonIdentityInfo(generator = ObjectIdGenerators.StringIdGenerator.class)
-public class LimeLightAngularToRelevantComponent implements DoubleSupplier {
-
-    @NotNull
-    AnalogPIDCommand anComd;
+public class LimeLightAngularToRelevantComponent extends LimeLightComponent implements DoubleSupplier {
 
     @JsonCreator
-    public LimeLightAngularToRelevantComponent(@JsonProperty(required = true) AnalogPIDCommand anComd){
-        this.anComd = anComd;
-
-
+    public LimeLightAngularToRelevantComponent(@JsonProperty(required = true) ReturnValue value){
+       super(value);
     }
 
     @Override
     public double getAsDouble() {
+        double returnAngle = super.getAsDouble();
         return 0;
     }
 }
