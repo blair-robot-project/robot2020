8fe9ceb4632b8a07413b5319c396f92cd0ba5932
Natenael Kelkay <natikelkay@gmail.com>
2019-02-15 17:18:56 -0500

    code for getting average of the yaw value

--- src/main/java/org/usfirst/frc/team449/robot/components/LimeLightYawTargetComponent.java
+++ src/main/java/org/usfirst/frc/team449/robot/components/LimeLightYawTargetComponent.java
@@ -4,5 +4,22 @@ import java.nio.DoubleBuffer;
 import java.util.function.DoubleSupplier;
 
 public class LimeLightYawTargetComponent implements DoubleSupplier {
+    DoubleBuffer buffer;
+    LimeLightComponent limeLightComponent;
 
+    public LimeLightYawTargetComponent(int bufferCapacity, LimeLightComponent limeLightComponent) {
+        buffer = DoubleBuffer.allocate(bufferCapacity);
+        this.limeLightComponent = limeLightComponent;
+    }
+
+    @Override
+    public double getAsDouble() {
+        buffer.put(limeLightComponent.getAsDouble());
+        double[] bufferArray = buffer.array();
+        double arraySum = 0;
+        for (int i = 0; i < bufferArray.length; i++){
+            arraySum +=  bufferArray[i];
+        }
+        return arraySum/bufferArray.length;
+    }
 }
