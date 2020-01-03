def4c47b0007d4df0f997dba9bf09cb4bb689cb7
unknown <james20902@gmail.com>
2020-01-03 07:38:02 -0500

    changed feedforwardcalculator type info, verified working

--- src/main/java/org/usfirst/frc/team449/robot/jacksonWrappers/MappedFeedForwardCalculator.java
+++ src/main/java/org/usfirst/frc/team449/robot/jacksonWrappers/MappedFeedForwardCalculator.java
@@ -1,15 +1,12 @@
 package org.usfirst.frc.team449.robot.jacksonWrappers;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.fasterxml.jackson.annotation.*;
 import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
 
 /**
  * Motor feed forward calculator using velocity and acceleration
  */
-@JsonIdentityInfo(generator = ObjectIdGenerators.StringIdGenerator.class)
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.WRAPPER_OBJECT, property = "@class")
 public class MappedFeedForwardCalculator extends SimpleMotorFeedforward {
 
     /**
