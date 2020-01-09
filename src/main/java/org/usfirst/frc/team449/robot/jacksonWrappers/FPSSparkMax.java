a4ca359e3430c75c69e142fbfef7c539ec619347
unknown <james20902@gmail.com>
2020-01-08 22:37:52 -0500

    started work on FPSSmartMotor interface

---
+++ src/main/java/org/usfirst/frc/team449/robot/jacksonWrappers/FPSSparkMax.java
@@ -0,0 +1,247 @@
+package org.usfirst.frc.team449.robot.jacksonWrappers;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import io.github.oblarg.oblog.Loggable;
+import io.github.oblarg.oblog.annotations.Log;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.usfirst.frc.team449.robot.generalInterfaces.FPSSmartMotor;
+import org.usfirst.frc.team449.robot.generalInterfaces.shiftable.Shiftable;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@JsonIdentityInfo(generator = ObjectIdGenerators.StringIdGenerator.class)
+public class FPSSparkMax implements FPSSmartMotor {
+
+    /**
+     * REV brushless controller object
+     */
+    private CANSparkMax spark;
+
+    /**
+     * REV provided encoder object
+     */
+    private CANEncoder canEncoder;
+
+    /**
+     * REV provided PID Controller
+     */
+    private CANPIDController pidController;
+
+    /**
+     * The counts per rotation of the encoder being used, or null if there is no encoder.
+     */
+    @Nullable
+    private final Integer encoderCPR;
+    /**
+     * The coefficient the output changes by after being measured by the encoder, e.g. this would be 1/70 if there was a
+     * 70:1 gearing between the encoder and the final output.
+     */
+    private final double postEncoderGearing;
+    /**
+     * The number of feet travelled per rotation of the motor this is attached to, or null if there is no encoder.
+     */
+    private final double feetPerRotation;
+    /**
+     * RPS as used in a unit conversion method. Field to avoid garbage collection.
+     */
+    private Double RPS;
+
+    /**
+     * The settings currently being used by this controller.
+     */
+    @NotNull
+    protected PerGearSettings currentGearSettings;
+    /**
+     * A list of all the gears this robot has and their settings.
+     */
+    @NotNull
+    private final Map<Integer, PerGearSettings> perGearSettings;
+
+    /**
+     * Create a new SPARK MAX Controller
+     *  @param deviceID The device ID.
+     * @param postEncoderGearing
+     * @param feetPerRotation
+     */
+    public FPSSparkMax(int deviceID,
+                       double postEncoderGearing,
+                       double feetPerRotation,
+                       @Nullable List<PerGearSettings> perGearSettings,
+                       @Nullable Shiftable.gear startingGear,
+                       @Nullable Integer startingGearNum) {
+        spark = new CANSparkMax(deviceID, CANSparkMaxLowLevel.MotorType.kBrushless);
+        canEncoder = spark.getEncoder();
+
+        this.encoderCPR = canEncoder.getCountsPerRevolution();
+        this.postEncoderGearing = postEncoderGearing;
+        this.feetPerRotation = feetPerRotation;
+
+        //Initialize
+        this.perGearSettings = new HashMap<>();
+
+        //If given no gear settings, use the default values.
+        if (perGearSettings == null || perGearSettings.size() == 0) {
+            this.perGearSettings.put(0, new PerGearSettings());
+        }
+        //Otherwise, map the settings to the gear they are.
+        else {
+            for (PerGearSettings settings : perGearSettings) {
+                this.perGearSettings.put(settings.gear, settings);
+            }
+        }
+        int currentGear;
+        //If the starting gear isn't given, assume we start in low gear.
+        if (startingGear == null) {
+            if (startingGearNum == null) {
+                currentGear = Integer.MAX_VALUE;
+                for (Integer gear : this.perGearSettings.keySet()) {
+                    if (gear < currentGear) {
+                        currentGear = gear;
+                    }
+                }
+            } else {
+                currentGear = startingGearNum;
+            }
+        } else {
+            currentGear = startingGear.getNumVal();
+        }
+        currentGearSettings = this.perGearSettings.get(currentGear);
+    }
+
+    @Override
+    public int getGear() {
+        return 0;
+    }
+
+    @Override
+    public void setGear(int gear) {
+
+    }
+
+    /**
+     * Convert from native units read by an encoder to feet moved. Note this DOES account for post-encoder gearing.
+     *
+     * @param revs revolutions measured by the encoder
+     * @return That distance in feet, or null if no encoder CPR was given.
+     */
+    @Override
+    public double encoderToFeet(double revs) {
+        return revs * feetPerRotation * postEncoderGearing;
+    }
+
+    /**
+     * Convert a distance from feet to encoder reading in native units. Note this DOES account for post-encoder
+     * gearing.
+     *
+     * @param feet A distance in feet.
+     * @return That distance in native units as measured by the encoder, or null if no encoder CPR was given.
+     */
+    @Override
+    public double feetToEncoder(double feet) {
+        return feet / feetPerRotation / postEncoderGearing;
+    }
+
+    /**
+     * Converts the velocity read by the talon's getVelocity() method to the FPS of the output shaft. Note this DOES
+     * account for post-encoder gearing.
+     *
+     * @param encoderReading The velocity read from the encoder with no conversions.
+     * @return The velocity of the output shaft, in FPS, when the encoder has that reading, or null if no encoder CPR
+     * was given.
+     */
+    @Override
+    public double encoderToFPS(double encoderReading) {
+        RPS = nativeToRPS(encoderReading);
+        return RPS * postEncoderGearing * feetPerRotation;
+    }
+
+    /**
+     * Converts from the velocity of the output shaft to what the talon's getVelocity() method would read at that
+     * velocity. Note this DOES account for post-encoder gearing.
+     *
+     * @param FPS The velocity of the output shaft, in FPS.
+     * @return What the raw encoder reading would be at that velocity, or null if no encoder CPR was given.
+     */
+    @Override
+    public double FPSToEncoder(double FPS) {
+        return RPSToNative((FPS / postEncoderGearing) / feetPerRotation);
+    }
+
+    /**
+     * Convert from CANTalon native velocity units to output rotations per second. Note this DOES NOT account for
+     * post-encoder gearing.
+     *
+     * @param nat A velocity in RPM
+     * @return That velocity in RPS
+     */
+    @Contract(pure = true)
+    @Override
+    public Double nativeToRPS(double nat) {
+        return nat / 60.;
+    }
+
+    /**
+     * Convert from output RPS to the CANTalon native velocity units. Note this DOES NOT account for post-encoder
+     * gearing.
+     *
+     * @param RPS The RPS velocity you want to convert.
+     * @return That velocity in RPM
+     */
+    @Contract(pure = true)
+    @Override
+    public double RPSToNative(double RPS) {
+        return RPS * 60.;
+    }
+
+    /**
+     * Get the velocity of the CANTalon in FPS.
+     *
+     * @return The CANTalon's velocity in FPS, or null if no encoder CPR was given.
+     */
+    @Log
+    public Double getVelocity() {
+        return encoderToFPS(canEncoder.getVelocity());
+    }
+
+    /**
+     * Set the velocity for the motor to go at.
+     *
+     * @param velocity the desired velocity, on [-1, 1].
+     */
+    @Override
+    public void setVelocity(double velocity) {
+        if (currentGearSettings.maxSpeed != null) {
+            setVelocityFPS(velocity * currentGearSettings.maxSpeed);
+        } else {
+            spark.set(velocity);
+        }
+    }
+
+    /**
+     * Give a velocity closed loop setpoint in FPS.
+     *
+     * @param velocity velocity setpoint in FPS.
+     */
+    public void setVelocityFPS(double velocity) {
+//        nativeSetpoint = FPSToEncoder(velocity);
+//        setpoint = velocity;
+//        canTalon.config_kF(0, 0, 0);
+//        canTalon.set(ControlMode.Velocity, nativeSetpoint, DemandType.ArbitraryFeedForward,
+//                currentGearSettings.getFeedForwardCalculator().calculate(velocity) / 12.);
+    }
+
+
+    @Override
+    public void disable() {
+        spark.disable();
+    }
+}
--- src/main/java/org/usfirst/frc/team449/robot/jacksonWrappers/FPSTalon.java
+++ src/main/java/org/usfirst/frc/team449/robot/jacksonWrappers/FPSTalon.java
@@ -18,9 +18,10 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.usfirst.frc.team449.robot.components.RunningLinRegComponent;
+import org.usfirst.frc.team449.robot.generalInterfaces.FPSSmartMotor;
 import org.usfirst.frc.team449.robot.generalInterfaces.shiftable.Shiftable;
 import org.usfirst.frc.team449.robot.generalInterfaces.simpleMotor.SimpleMotor;
-import org.usfirst.frc.team449.robot.generalInterfaces.smartMotor.PerGearSettings;
+import org.usfirst.frc.team449.robot.jacksonWrappers.FeedForwardCalculators.MappedFeedForwardCalculator;
 
 
 import java.util.HashMap;
@@ -32,7 +33,7 @@ import java.util.Map;
  * in this class takes arguments in post-gearing FPS.
  */
 @JsonIdentityInfo(generator = ObjectIdGenerators.StringIdGenerator.class)
-public class FPSTalon implements SimpleMotor, Shiftable, Loggable {
+public class FPSTalon implements FPSSmartMotor {
 
     Faults faults = new Faults();
 
@@ -416,7 +417,8 @@ public class FPSTalon implements SimpleMotor, Shiftable, Loggable {
      * @param nativeUnits A distance native units as measured by the encoder.
      * @return That distance in feet, or null if no encoder CPR was given.
      */
-    protected double encoderToFeet(double nativeUnits) {
+    @Override
+    public double encoderToFeet(double nativeUnits) {
         if (encoderCPR == null) {
             return Double.NaN;
         }
@@ -430,7 +432,8 @@ public class FPSTalon implements SimpleMotor, Shiftable, Loggable {
      * @param feet A distance in feet.
      * @return That distance in native units as measured by the encoder, or null if no encoder CPR was given.
      */
-    protected double feetToEncoder(double feet) {
+    @Override
+    public double feetToEncoder(double feet) {
         if (encoderCPR == null) {
             return Double.NaN;
         }
@@ -445,7 +448,8 @@ public class FPSTalon implements SimpleMotor, Shiftable, Loggable {
      * @return The velocity of the output shaft, in FPS, when the encoder has that reading, or null if no encoder CPR
      * was given.
      */
-    protected double encoderToFPS(double encoderReading) {
+    @Override
+    public double encoderToFPS(double encoderReading) {
         RPS = nativeToRPS(encoderReading);
         if (RPS == null) {
             return Double.NaN;
@@ -460,7 +464,8 @@ public class FPSTalon implements SimpleMotor, Shiftable, Loggable {
      * @param FPS The velocity of the output shaft, in FPS.
      * @return What the raw encoder reading would be at that velocity, or null if no encoder CPR was given.
      */
-    protected double FPSToEncoder(double FPS) {
+    @Override
+    public double FPSToEncoder(double FPS) {
         return RPSToNative((FPS / postEncoderGearing) / feetPerRotation);
     }
 
@@ -473,7 +478,8 @@ public class FPSTalon implements SimpleMotor, Shiftable, Loggable {
      */
     @Contract(pure = true)
     @Nullable
-    private Double nativeToRPS(double nat) {
+    @Override
+    public Double nativeToRPS(double nat) {
         if (encoderCPR == null) {
             return null;
         }
@@ -488,7 +494,8 @@ public class FPSTalon implements SimpleMotor, Shiftable, Loggable {
      * @return That velocity in CANTalon native units, or null if no encoder CPR was given.
      */
     @Contract(pure = true)
-    private double RPSToNative(double RPS) {
+    @Override
+    public double RPSToNative(double RPS) {
         if (encoderCPR == null) {
             return Double.NaN;
         }
@@ -514,6 +521,7 @@ public class FPSTalon implements SimpleMotor, Shiftable, Loggable {
      * @return The CANTalon's velocity in FPS, or null if no encoder CPR was given.
      */
     @Log
+    @Override
     public Double getVelocity() {
         return encoderToFPS(canTalon.getSelectedSensorVelocity(0));
     }
@@ -537,7 +545,8 @@ public class FPSTalon implements SimpleMotor, Shiftable, Loggable {
      *
      * @param velocity velocity setpoint in FPS.
      */
-    protected void setVelocityFPS(double velocity) {
+    @Override
+    public void setVelocityFPS(double velocity) {
         nativeSetpoint = FPSToEncoder(velocity);
         setpoint = velocity;
         canTalon.config_kF(0, 0, 0);
@@ -608,14 +617,6 @@ public class FPSTalon implements SimpleMotor, Shiftable, Loggable {
     @Log
     public String getControlMode() {
         return String.valueOf(canTalon.getControlMode());
-    }
-
-    /**
-     * Enables the motor, if applicable.
-     */
-    @Override
-    public void enable() {
-        //Not a thing anymore
     }
 
     /**
@@ -776,5 +777,224 @@ public class FPSTalon implements SimpleMotor, Shiftable, Loggable {
     @Override
     public LayoutType configureLayoutType() {
         return BuiltInLayouts.kGrid;
+    }
+
+    /**
+     * An object representing the CANTalon settings that are different for each gear.
+     */
+    class PerGearSettings {
+
+        /**
+         * The gear number this is the settings for.
+         */
+        public final int gear;
+
+        /**
+         * The forwards and reverse peak output voltages.
+         */
+        public final double fwdPeakOutputVoltage, revPeakOutputVoltage;
+
+        /**
+         * The forwards and reverse nominal output voltages.
+         */
+        public final double fwdNominalOutputVoltage, revNominalOutputVoltage;
+
+        /**
+         * The ramp rate, in volts/sec. null means no ramp rate.
+         */
+        @Nullable
+        public final Double rampRate;
+
+        /**
+         * The maximum speed of the motor in this gear, in FPS. Used for throttle scaling.
+         */
+        @Nullable
+        public final Double maxSpeed;
+
+        /**
+         * The PID constants for the motor in this gear. Ignored if maxSpeed is null.
+         */
+        public final double kP, kI, kD;
+
+        /**
+         * The position PID constants for the motor in this gear.
+         */
+        public final double posKP, posKI, posKD;
+
+        /**
+         * WPI object for calculating feed forward constants given a max achievable velocity
+         * and acceleration
+         */
+        public SimpleMotorFeedforward feedForwardCalculator;
+
+        /**
+         * Default constructor.
+         *
+         * @param gearNum                 The gear number this is the settings for. Ignored if gear isn't null.
+         * @param gear                    The gear this is the settings for. Can be null.
+         * @param fwdPeakOutputVoltage    The peak output voltage for closed-loop modes in the forwards direction, in
+         *                                volts. Defaults to 12.
+         * @param revPeakOutputVoltage    The peak output voltage for closed-loop modes in the reverse direction, in
+         *                                volts. Defaults to -fwdPeakOutputVoltage.
+         * @param fwdNominalOutputVoltage The minimum output voltage for closed-loop modes in the forwards direction.
+         *                                This does not rescale, it just sets any output below this voltage to this
+         *                                voltage. Defaults to 0.
+         * @param revNominalOutputVoltage The minimum output voltage for closed-loop modes in the reverse direction.
+         *                                This does not rescale, it just sets any output below this voltage to this
+         *                                voltage. Defaults to -fwdNominalOutputVoltage.
+         *
+         * @param feedForwardCalculator   The component for calculating feedforwards in closed-loop control modes.
+         *
+         * @param rampRate                The ramp rate, in volts/sec. Can be null, and if it is, no ramp rate is used.
+         * @param maxSpeed                The maximum speed of the motor in this gear, in FPS. Used for throttle
+         *                                scaling. Ignored if kVFwd is null. Calculated from the drive characterization
+         *                                terms if null.
+         * @param kP                      The proportional PID constant for the motor in this gear. Ignored if kVFwd is
+         *                                null. Defaults to 0.
+         * @param kI                      The integral PID constant for the motor in this gear. Ignored if kVFwd is
+         *                                null. Defaults to 0.
+         * @param kD                      The derivative PID constant for the motor in this gear. Ignored if kVFwd is
+         *                                null. Defaults to 0.
+         * @param posKP                   The proportional PID constant for position control on the motor in this gear. Ignored if kVFwd is
+         *                                null. Defaults to 0.
+         * @param posKI                   The integral PID constant for position control on the motor in this gear. Ignored if kVFwd is
+         *                                null. Defaults to 0.
+         * @param posKD                   The derivative PID constant for position control on the motor in this gear. Ignored if kVFwd is
+         *                                null. Defaults to 0.
+         */
+        @JsonCreator
+        public PerGearSettings(int gearNum,
+                               @Nullable Shiftable.gear gear,
+                               @Nullable Double fwdPeakOutputVoltage,
+                               @Nullable Double revPeakOutputVoltage,
+                               @Nullable Double fwdNominalOutputVoltage,
+                               @Nullable Double revNominalOutputVoltage,
+                               @Nullable MappedFeedForwardCalculator feedForwardCalculator,
+                               @Nullable Double rampRate,
+                               @Nullable Double maxSpeed,
+                               double kP,
+                               double kI,
+                               double kD,
+                               double posKP,
+                               double posKI,
+                               double posKD) {
+            this.gear = gear != null ? gear.getNumVal() : gearNum;
+            this.fwdPeakOutputVoltage = fwdPeakOutputVoltage != null ? fwdPeakOutputVoltage : 12;
+            this.revPeakOutputVoltage = revPeakOutputVoltage != null ? revPeakOutputVoltage :
+                    -this.fwdPeakOutputVoltage;
+            this.fwdNominalOutputVoltage = fwdNominalOutputVoltage != null ? fwdNominalOutputVoltage : 0;
+            this.revNominalOutputVoltage = revNominalOutputVoltage != null ? revNominalOutputVoltage :
+                    -this.fwdNominalOutputVoltage;
+            this.feedForwardCalculator = feedForwardCalculator != null ? feedForwardCalculator :
+                    new SimpleMotorFeedforward(0,0);
+            this.rampRate = rampRate;
+            this.kP = kP;
+            this.kI = kI;
+            this.kD = kD;
+            this.posKP = posKP;
+            this.posKI = posKI;
+            this.posKD = posKD;
+            this.maxSpeed = maxSpeed;
+        }
+
+        /**
+         * Empty constructor that uses all default options.
+         */
+        public PerGearSettings() {
+            this(0, null, null, null, null, null, null, null, null, 0, 0, 0, 0, 0 ,0);
+        }
+
+        /**
+         * @return The gear number this is the settings for.
+         */
+        public int getGear() {
+            return gear;
+        }
+
+        /**
+         * @return The peak output voltage for closed-loop modes in the forwards direction, in volts.
+         */
+        public double getFwdPeakOutputVoltage() {
+            return fwdPeakOutputVoltage;
+        }
+
+        /**
+         * @return The peak output voltage for closed-loop modes in the reverse direction, in volts.
+         */
+        public double getRevPeakOutputVoltage() {
+            return revPeakOutputVoltage;
+        }
+
+        /**
+         * @return The minimum output voltage for closed-loop modes in the forwards direction. This does not rescale, it
+         * just sets any output below this voltage to this voltage.
+         */
+        public double getFwdNominalOutputVoltage() {
+            return fwdNominalOutputVoltage;
+        }
+
+        /**
+         * @return The minimum output voltage for closed-loop modes in the reverse direction. This does not rescale, it
+         * just sets any output below this voltage to this voltage.
+         */
+        public double getRevNominalOutputVoltage() {
+            return revNominalOutputVoltage;
+        }
+
+        /**
+         * @return Feedforward calculator for this gear
+         */
+        public SimpleMotorFeedforward getFeedForwardCalculator(){
+            return feedForwardCalculator;
+        }
+
+        /**
+         * @return The ramp rate, in volts/sec.
+         */
+        @Nullable
+        public Double getRampRate() {
+            return rampRate;
+        }
+
+        /**
+         * @return The maximum speed of the motor in this gear, in FPS.
+         */
+        @Nullable
+        public Double getMaxSpeed() {
+            return maxSpeed;
+        }
+
+        /**
+         * @return The proportional PID constant for the motor in this gear.
+         */
+        public double getkP() {
+            return kP;
+        }
+
+        /**
+         * @return The integral PID constant for the motor in this gear.
+         */
+        public double getkI() {
+            return kI;
+        }
+
+        /**
+         * @return The derivative PID constant for the motor in this gear.
+         */
+        public double getkD() {
+            return kD;
+        }
+
+        public double getPosKP() {
+            return posKP;
+        }
+
+        public double getPosKI() {
+            return posKI;
+        }
+
+        public double getPosKD() {
+            return posKD;
+        }
     }
 }
