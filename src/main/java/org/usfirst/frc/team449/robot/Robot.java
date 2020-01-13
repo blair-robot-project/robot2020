265063e68769dcd2a5c42f246b673906b809544a
unknown <james20902@gmail.com>
2020-01-13 13:22:38 -0500

    trigger shuffleboard recording

--- src/main/java/org/usfirst/frc/team449/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team449/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import io.github.oblarg.oblog.Logger;
@@ -89,6 +90,8 @@ public class Robot extends TimedRobot {
         }
 
         Logger.configureLoggingAndConfig(robotMap, false);
+        Shuffleboard.setRecordingFileNameFormat("log-${time}");
+        Shuffleboard.startRecording();
 
         //start systems
         if (robotMap.getRobotStartupCommands() != null) {
