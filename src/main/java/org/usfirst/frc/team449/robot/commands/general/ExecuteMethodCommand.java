f15c6ec85e315758e0d5af48ff4d1d8b31d8c5e3
ysthakur <45539777+ysthakur@users.noreply.github.com>
2020-01-13 18:33:16 -0500

    Added spinUpFlywheel commands, supposed to simplify, didn't actually. Changed test.yml
    Signed-off-by: ysthakur <45539777+ysthakur@users.noreply.github.com>

---
+++ src/main/java/org/usfirst/frc/team449/robot/commands/general/ExecuteMethodCommand.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team449.robot.commands.general;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import org.usfirst.frc.team449.robot.other.Util;
+
+import java.util.List;
+
+public class ExecuteMethodCommand extends InstantCommand {
+
+    public ExecuteMethodCommand(Object object, String methodName, List<Subsystem> requirements) {
+        super(Util.getMethod(object, methodName), (Subsystem[]) requirements.toArray());
+    }
+}
---
+++ src/main/java/org/usfirst/frc/team449/robot/subsystem/interfaces/flywheel/commands/SpinUpFlywheelUntilIntakeStops.java
@@ -0,0 +1,75 @@
+package org.usfirst.frc.team449.robot.subsystem.interfaces.flywheel.commands;
+
+import edu.wpi.first.wpilibj2.command.*;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import org.usfirst.frc.team449.robot.CommandContainer;
+import org.usfirst.frc.team449.robot.commands.general.MappedSequentialCommandGroup;
+import org.usfirst.frc.team449.robot.commands.general.MappedWaitCommand;
+import org.usfirst.frc.team449.robot.commands.general.ConditionalCommandFunctional;
+import org.usfirst.frc.team449.robot.subsystem.interfaces.flywheel.commands.TurnAllOn;
+import org.usfirst.frc.team449.robot.subsystem.complex.shooter.LoggingFlywheel;
+import org.usfirst.frc.team449.robot.subsystem.interfaces.flywheel.commands.SpinUpFlywheel;
+import org.usfirst.frc.team449.robot.subsystem.interfaces.intake.IntakeSimple;
+import org.usfirst.frc.team449.robot.subsystem.interfaces.intake.SubsystemIntake;
+
+import java.util.*;
+
+/**
+ * Just all the stuff Yueqin added to the map for the feeder
+ * put into one command, for ease of readability
+ * TODO test this out
+ */
+public class SpinUpFlywheelUntilIntakeStops extends SequentialCommandGroup implements ISpinUpFlywheelCommand {
+
+    Command x;
+    /**
+     *
+      * @param timeToStart how long the feeder waits to start. Not sure how necessary this is
+     * @param spinUpFlywheelCommand
+     * @param shooterFlywheel
+     * @param intake the intake
+     * @param timeToStop how long the feeder keeps running after intake stops, in sec
+     */
+  public SpinUpFlywheelUntilIntakeStops(@Nullable Double timeToStart,
+                                        @NotNull final SpinUpFlywheel spinUpFlywheelCommand,
+                                        @NotNull final LoggingFlywheel shooterFlywheel,
+                                        @NotNull final IntakeSimple intake,
+                                        @Nullable Double timeToStop) {
+    super(new MappedWaitCommand(timeToStart == null ? 1.0 : timeToStart),
+            makePerpetualCommand(spinUpFlywheelCommand, shooterFlywheel, intake, timeToStop));
+  }
+
+  public static Command makePerpetualCommand(@NotNull final SpinUpFlywheel spinUpFlywheelCommand,
+                   @NotNull final LoggingFlywheel shooterFlywheel,
+                   @NotNull final IntakeSimple intake,
+                   @Nullable Double timeToStop) {
+      //todo should this be a perpetual command?
+      return new PerpetualCommand(
+              //TODO make this less complicated, this is even worse than before
+              new ConditionalCommandFunctional(new CommandBase() {
+                  Runnable endCommand = () -> {
+                          this.end(false);
+                  };
+                  @Override
+                  public void execute() {
+                      //stop after timeToStop seconds
+                      Timer timer = new Timer();
+                      timer.schedule(new TimerTask() {
+                          public void run() {
+                            //TODO is it interrupted? Does making it false keep it from starting up again?
+                            endCommand.run();
+                          }
+                      }, (long) (timeToStop == null ? 3000 : timeToStop * 1000L));
+                  }
+              }, new ConditionalCommandFunctional(
+                      new TurnAllOn(shooterFlywheel),
+                      spinUpFlywheelCommand,
+                      shooterFlywheel::isConditionTrueCached),
+                      //whether or not it is off
+                      () -> intake.getMode().compareTo(SubsystemIntake.IntakeMode.OFF) == 0)
+      );
+  }
+
+}
